92801484af71b9131d63dd6c07dc858661fc8b59
makasud tatakoti <makasudtatakoti@gmail.com>
2026-05-18 18:12:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 function requestHandler(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("chapri");
+  res.end("this is my pipeline");
 }
 
 const server = http.createServer(requestHandler);
